3b388213ba5357ecbfdaab60889d7ca7ee08fed5
sazid-zero <sharif.sazid.3@gmail.com>
2025-06-30 15:46:13 +0600

    Update vite.ts

--- server/vite.ts
+++ server/vite.ts
@@ -45,12 +45,8 @@ export async function setupVite(app: Express, server: Server) {
     const url = req.originalUrl;
 
     try {
-      const clientTemplate = path.resolve(
-        import.meta.dirname,
-        "..",
-        "client",
-        "index.html",
-      );
+      const rootDir = import.meta.dirname || process.cwd();
+      const clientTemplate = path.join(rootDir, "..", "client", "index.html");
 
       // always reload the index.html file from disk incase it changes
       let template = await fs.promises.readFile(clientTemplate, "utf-8");
